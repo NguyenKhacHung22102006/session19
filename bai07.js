e5389baac265017f39d95be5095d36f370ce40bb
NguyenKhacHung22102006 <hungskyok06@gmail.com>
2025-03-03 22:58:28 +0700

    Add files via upload

--- bai07.js
+++ bai07.js
@@ -1,9 +1,10 @@
-let max = Math.max(a, b, c);
+
 let a = parseFloat(prompt("Nhập số thứ nhất:"));
 let b = parseFloat(prompt("Nhập số thứ hai:"));
 let c = parseFloat(prompt("Nhập số thứ ba:"));
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert("Vui lòng nhập đúng 3 số hợp lệ!");
 } else {
+    let max = Math.max(a, b, c);
     alert(`Số lớn nhất trong 3 số là: ${max}`);
 }
